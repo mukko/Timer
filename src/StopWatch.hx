82a70c7a6cf8945a54201ffb766898dcddc8e5ef
mukko <kyo22943@gol.com>
2014-11-16 23:55:21 +0900

    start,stopメソッドを追加
    ボタンを押した時のupdateメソッドが必要になりそう

--- src/StopWatch.hx
+++ src/StopWatch.hx
@@ -7,20 +7,32 @@ package ;
 import flash.utils.Timer;
 class StopWatch
 {
-	static private var timer : Timer;
-	
+	static private var timer:Timer;
+
 	//コンストラクタではタイマーオブジェクトを作成するだけ
-	public static function new()
+
+	public function new()
 	{
 		timer = new Timer(0);
 	}
-	
+
 	/**
 	*タイマーを実行する時間をセットする
 	* @param	delay	タイマー時間(ms)
 **/
+
 	public function delaySetter(delay:Float):Void
 	{
-		timer.delay(delay);
+		timer.delay = delay;
+	}
+
+	public function Starter():Void
+	{
+		timer.start();
+	}
+
+	public function Stopper():Void
+	{
+		timer.stop();
 	}
 }

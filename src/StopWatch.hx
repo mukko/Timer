6526a9391c95aec30d423026285eadac70c08e1c
mukko <kyo22943@gol.com>
2014-11-21 23:56:21 +0900

    コメントを追加

--- src/StopWatch.hx
+++ src/StopWatch.hx
@@ -11,31 +11,34 @@ import flash.utils.Timer;
 class StopWatch
 {
 	static private var timer:Timer;
-	private var loop:Int = 20000000;
-	private var number:Int = 1;
+	private var loop:Int = 20000000; //ループ回数
+	private var count:Int = 1; //カウント用
 	private var sound1:Sound = new Sound(new URLRequest("../res/button02a.mp3"));
 	private var sound2:Sound = new Sound(new URLRequest("../res/button01a.mp3"));
-	
+
 	/**
 	*コンストラクタは1つのみ
 	* timerのインスタンスを作成し、それをイベントに登録する
 	**/
+
 	public function new()
 	{
 		timer = new Timer(0);
-		timer.addEventListener(TimerEvent.TIMER,function(e:TimerEvent):Void
-	{
-		if((number % 5) == 0){
-			sound2.play();
-		}else{
-			sound1.play();
-		}
-		number++;
-	});
-		timer.addEventListener(TimerEvent.TIMER_COMPLETE ,function (e:TimerEvent):Void
-	{
-		trace("done!");
-	});
+		timer.addEventListener(TimerEvent.TIMER, function(e:TimerEvent):Void
+		{
+			if ((count % 5) == 0)
+			{
+				sound2.play();
+			} else
+			{
+				sound1.play();
+			}
+			count++;
+		});
+		timer.addEventListener(TimerEvent.TIMER_COMPLETE, function(e:TimerEvent):Void
+		{
+			trace("done!");
+		});
 	}
 
 	/**
@@ -48,19 +51,28 @@ class StopWatch
 		timer.delay = delay;
 		timer.repeatCount = loop;
 	}
-	
+	/**
+	*タイマーをスタートさせる
+**/
+
 	public function Starter():Void
 	{
 		timer.start();
 	}
-	
+	/**
+	*タイマーをストップする
+**/
+
 	public function Stopper():Void
 	{
 		timer.stop();
 	}
-	
+	/**
+	*updateは動画をしたいわけではないので空です
+**/
+
 	public function update():Void
 	{
-	
+
 	}
 }
